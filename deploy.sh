148c1c48374cc93b86ef46e3ce3efe404e9af800
Michael Oni <olalekan.oni02@gmail.com>
2023-03-22 14:36:33 -0500

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@ Deploy python apps
 deploy in the dev env
 deployment5 main
 deployment4
+deploy to uat
+deploy to staging env
+echo good job
+tested and accepted
+(tested and approved by the client's QA team)
